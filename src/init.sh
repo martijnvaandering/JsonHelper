cc10ed8e88eadb4a60974a970dddc036edd8d640
Martijn Vaandering <martijn.vaandering@cegeka.com>
2023-04-04 11:05:45 +0200

    reduce startup time

--- src/init.sh
+++ src/init.sh
@@ -4,9 +4,8 @@ if [ $cli = "true" ]; then
     bash
     exit 0;
 fi
-/etc/init.d/nginx start
-/etc/init.d/fcgiwrap restart
+/etc/init.d/fcgiwrap start
 chgrp nginx /var/run/fcgiwrap.socket
 chmod g+w /var/run/fcgiwrap.socket
-/etc/init.d/nginx restart
+/etc/init.d/nginx start
 tail -f /var/log/nginx/access.log -f /var/log/nginx/error.log
